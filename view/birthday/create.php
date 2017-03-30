b82c429b05a9c56f9f11cc14f5b0b5d79f8cd647
Jis van Overschot <99040091@mydavinci.nl>
2017-03-30 11:25:58 +0200

    chnged send button text

--- view/birthday/create.php
+++ view/birthday/create.php
@@ -2,6 +2,6 @@
 	<form action="<?= URL ?>birthday/createSave" method="post">
 		<input type="date" name="date">
 		<input type="text" name="person" placeholder="Naam">
-		<input type="submit" value="Verzenden">
+		<input type="submit" value="Toevoegen">
 	</form>
 </div>
