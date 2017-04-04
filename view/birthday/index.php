266fc7547e7571e2daabb4deeeeead2720f928bf
Jis van Overschot <99040091@mydavinci.nl>
2017-04-04 13:23:03 +0200

    fixed grouping by month and day.

--- view/birthday/index.php
+++ view/birthday/index.php
@@ -1,15 +1,29 @@
     <h1 class="center-align"><b>Kalender</b></h1>
 <?php
     setlocale(LC_TIME, 'Dutch_Netherlands', 'Dutch', 'nl_NL', 'nl', 'nl_NL.ISO8859-1', 'nl_NL.UTF-8', 'nld_nld', 'nld', 'nld_NLD', 'NL_nl');
+    $currentMonth = null;
+    $currentDay = null;
     foreach ($birthdays as $birthday) {
-    $monthNum  = $birthday['birthday_month'];
-    $monthName = strftime("%B", mktime(0, 0, 0, $monthNum, 10));
+        $monthNum  = $birthday['birthday_month'];
+        $monthName = strftime("%B", mktime(0, 0, 0, $monthNum, 10));
 ?>
     <div class="container">
         <div class="divider"></div>
         <div class="section">
-            <h2><?= $monthName ?></h2>
-            <h4><?= $birthday['birthday_day']; ?></h4>
+            <h2>
+                <?php if ($currentMonth != $monthName) {
+                    echo $monthName;
+                    $currentMonth = $monthName;
+                }
+                ?>
+            </h2>
+            <h4>
+                <?php if ($currentDay != $birthday['birthday_day']) {
+                    echo $birthday['birthday_day'];
+                    $currentDay = $birthday['birthday_day'];
+                }
+                ?>
+            </h4>
             <div class="row">
                 <div class="col s12 m3">
                     <div class="card">
